0ffdf3aa4c2b6441c729c88b120043147f09c0f0
Jose <joey_manuel@outlook.com>
2020-11-21 17:41:07 -0500

    5. Fifth Commit
    Applied the Player Walking Animation and blended it with the Player Script.

--- ARPG_Proejct/Assets/Scripts/Player.cs
+++ ARPG_Proejct/Assets/Scripts/Player.cs
@@ -6,12 +6,14 @@ public class Player : MonoBehaviour
 {
     #region Variables
     public float speed;
+
+    Animator anim;
     #endregion Variables
 
     // Use this for initialization
     void Start()
     {
-
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -26,21 +28,33 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(0, speed * Time.deltaTime, 0);
+            anim.SetInteger("dir", 0);
+            anim.speed = 1;
         }
 
         else if (Input.GetKey(KeyCode.S))
         {
             transform.Translate(0, -speed * Time.deltaTime, 0);
+            anim.SetInteger("dir", 1);
+            anim.speed = 1;
         }
 
         else if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(-speed * Time.deltaTime, 0, 0);
+            anim.SetInteger("dir", 2);
+            anim.speed = 1;
         }
 
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(speed * Time.deltaTime, 0 , 0);
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+            anim.SetInteger("dir", 3);
+            anim.speed = 1;
         }
+
+        // if no key is pressed anim parameter direction is 0
+        else
+            anim.speed = 0;
     }
 }
