ba9e3bd95a4084f256ac869269fe9f62abfcc773
Jose <joey_manuel@outlook.com>
2020-11-21 10:16:42 -0500

    3. third Commit
    in this commit, we created:
    - The Player Script (made the player move in the plane)
    - Created the Sprite folder.
    - Exported the Player Assets.

---
+++ ARPG_Proejct/Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    #region Variables
+    public float speed;
+    #endregion Variables
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+
+    void PlayerMovement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, speed * Time.deltaTime, 0);
+        }
+
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, -speed * Time.deltaTime, 0);
+        }
+
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(speed * Time.deltaTime, 0 , 0);
+        }
+    }
+}
